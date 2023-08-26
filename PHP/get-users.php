1ecd577ee23460f75f8479ffadcd3f9c86e28806
AlineJo <albaash247>
2023-08-26 16:43:30 +0400

    #ep-0015 #

--- PHP/get-users.php
+++ PHP/get-users.php
@@ -1,6 +1,6 @@
 <?php
 
-function getResponse(){
+function getResonse(){
 // Include the database connection file
 require 'db-connection.php';
 
@@ -16,7 +16,7 @@ return $result;
 
 function printResponse(){
 
-$jsonResponse = json_encode(getResponse());
+$jsonResponse = json_encode(getResonse());
 echo $jsonResponse;
 
 }
